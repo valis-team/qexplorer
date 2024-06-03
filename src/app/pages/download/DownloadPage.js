eddebc4c4cdde4a2c675e2599bbdf9665af41032
Denis <dngaspar5@gmail.com>
2024-06-03 15:03:35 +0000

    categorized downloads

--- src/app/pages/download/DownloadPage.js
+++ src/app/pages/download/DownloadPage.js
@@ -3,9 +3,18 @@ import { useEffect, useState } from 'react';
 import { LinearProgress } from '@mui/material';
 import DownloadButton from '../components/Download/DownloadButton';
 
+const categories = [
+  { name: 'All', label: 'All' },
+  { name: 'Windows', label: 'Windows' },
+  { name: 'macOS', label: 'macOS' },
+  { name: 'Linux', label: 'Linux' },
+  { name: 'Android', label: 'Apk' },
+];
+
 function DownloadPage() {
   const [releases, setRelease] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [systemType, setSystemType] = useState(categories[0].label);
 
   // Helper function to parse filename and extract information, including a human-readable date
   const parseFilename = (filename) => {
@@ -22,28 +31,35 @@ function DownloadPage() {
     return { productName, platform, date };
   };
 
-  useEffect(() => {
-    // Fetch release data from the server
-    const fetchReleases = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios.post(`${process.env.REACT_APP_DOWNLOAD_URL}/fetch-release`);
-        const sortedReleases = response.data.sort((a, b) => {
+  const fetchReleases = async (category) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_DOWNLOAD_URL}/fetch-release`);
+      const sortedReleases =
+        response.data.sort((a, b) => {
           const timestampA = parseInt(a.split('-').pop().split('.')[0], 10);
           const timestampB = parseInt(b.split('-').pop().split('.')[0], 10);
           return timestampB - timestampA; // For descending order
-        });
+        }) || [];
+      if (category === 'All') {
         setRelease(sortedReleases); // Assuming the response.data is an array of filenames
-        console.log(sortedReleases);
-      } catch (error) {
-        console.error('Failed to fetch releases:', error);
-      } finally {
-        setIsLoading(false);
+      } else {
+        const filteredRelease =
+          sortedReleases.filter((item) => item.toLowerCase().includes(category.toLowerCase())) ||
+          [];
+        setRelease(filteredRelease);
       }
-    };
+    } catch (error) {
+      console.error('Failed to fetch releases:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    fetchReleases();
-  }, []);
+  useEffect(() => {
+    // Fetch release data from the server
+    fetchReleases(systemType);
+  }, [systemType]);
 
   return isLoading ? (
     <div className="w-full md:w-4/5 absolute">
@@ -52,7 +68,23 @@ function DownloadPage() {
   ) : (
     <div className="container px-12 py-20 md:px-24 flex flex-col gap-10 max-h-[calc(100vh-76px)] overflow-y-auto">
       <h1 className="text-4xl font-bold text-center mb-8">Available Downloads</h1>
-      {releases.length !== 0 && (
+      <div className="flex gap-10">
+        {categories.map((category, idx) => {
+          return (
+            <button
+              onClick={() => setSystemType(category.label)}
+              type="button"
+              className={`cursor-pointer px-6 p-2 rounded  hover:bg-main-50 hover:text-white ${
+                systemType === category.label ? 'bg-main-50 text-white' : ''
+              }`}
+              key={idx}
+            >
+              {category.name}
+            </button>
+          );
+        })}
+      </div>
+      {releases && releases.length !== 0 && (
         <div className="space-y-4">
           {releases.map((filename, index) => {
             const { productName, platform, date } = parseFilename(filename);
